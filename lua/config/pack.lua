be4609e56da423b95d6732293a6b1b40c8fb8a05
Mariappan Ramasamy <1221719+nappairam@users.noreply.github.com>
2026-04-06 11:03:02 +0800

    move LSP configs to lsp directory

--- lua/config/pack.lua
+++ lua/config/pack.lua
@@ -21,24 +21,4 @@ require('mini.basics').setup()
 require('mini.pick').setup()
 require('mini.surround').setup()
 
-vim.lsp.config['lua_ls'] = {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-}
-
-vim.lsp.enable('lua_ls')
-
-vim.lsp.config.rust_analyzer = {
-    cmd = { 'rust-analyzer' },
-    filetypes = { 'rust' },
-    root_markers = {"Cargo.toml", ".git"},
-    single_file_support = true,
-    before_init = function(init_params, config)
-        -- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
-        if config.settings and config.settings['rust-analyzer'] then
-            init_params.initializationOptions = config.settings['rust-analyzer']
-        end
-    end,
-}
-vim.lsp.enable("rust_analyzer")
+vim.lsp.enable({ 'lua_ls', 'rust_analyzer' })
